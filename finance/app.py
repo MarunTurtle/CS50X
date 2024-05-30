ffa5968685fdeb8f91d342ecb1b2d8aaf76d901f
Jaehyun Lee <132056894+MarunTurtle@users.noreply.github.com>
2024-05-30 14:12:28 +0900

    update

--- finance/app.py
+++ finance/app.py
@@ -54,75 +54,6 @@ def register():
     else:
         return render_template("register.html")
 
-
-@app.route("/quote", methods=["GET", "POST"])
-@login_required
-def quote():
-    """Get stock quote."""
-    if request.method == "POST":
-        symbol = request.form.get("symbol")
-        quote = lookup(symbol)
-        if not quote:
-            return apology("invalid symbol", 400)
-        return render_template("quote.html", quote=quote)
-    else:
-        return render_template("quote.html")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@app.after_request
-def after_request(response):
-    """Ensure responses aren't cached"""
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
-
-
-@app.route("/")
-@login_required
-def index():
-    """Show portfolio of stocks"""
-    return apology("TODO")
-
-
-@app.route("/buy", methods=["GET", "POST"])
-@login_required
-def buy():
-    """Buy shares of stock"""
-    return apology("TODO")
-
-
-@app.route("/history")
-@login_required
-def history():
-    """Show history of transactions"""
-    return apology("TODO")
-
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
@@ -160,6 +91,104 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
+
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+def quote():
+    """Get stock quote."""
+    if request.method == "POST":
+        symbol = request.form.get("symbol")
+        quote = lookup(symbol)
+        if not quote:
+            return apology("invalid symbol", 400)
+        return render_template("quote.html", quote=quote)
+    else:
+        return render_template("quote.html")
+
+@app.route("/buy", methods=["GET", "POST"])
+@login_required
+def buy():
+    """Buy shares of stock"""
+    if request.method == "POST":
+        symbol = request.form.get("symbol").upper()
+        shares = request.form.get("shares")
+        if not symbol:
+            return apology("must insert symbol")
+        elif not shares or not shares.isdigit() or int(shares) <= 0:
+            return apology("must insert a positive integer number")
+        
+        quote = lookup(symbol)
+        if quote is None:
+            return apology("no such symbol exists")
+        
+        price = quote["price"]
+        total_cost = int(shares) * price
+        cash = db.execute("SELECT cash FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["cash"]
+
+        if cash < total_cost:
+            return apology("not enough cash")
+        
+        db.execute("UPDATE users SET cash = cahs - :total_cost WHERE id = :user_id",
+                   total_cost=total_cost, user_id=session["user_id"])
+        
+        db.execute("INSERT INTO transactions (user_id, symbol, shares, price) VALUES (:user_id :symbol, :shares, :price)",
+                   user_id=session["user_id"], symbol=symbol, shares=shares, price=price)
+        
+        flash(f"Successfully purchased {shares} shares of {symbol} for {usd(total_cost)}.")
+        return redirect("/")
+    
+    else:
+        return render_template("buy.html")
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+
+@app.route("/")
+@login_required
+def index():
+    """Show portfolio of stocks"""
+    return apology("TODO")
+
+
+
+
+
+@app.route("/history")
+@login_required
+def history():
+    """Show history of transactions"""
+    return apology("TODO")
+
+
+
+
 
 
 @app.route("/logout")
